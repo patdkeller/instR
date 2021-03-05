5d1a4c331dfb62c7b5e8a8859f2371c491c9424a
Patrick Keller <patrick@kelleranalytics.com>
2021-03-05 12:33:46 -0500

    Adding a function that makes datatables with some favorite parameters

---
+++ R/output_datatable.R
@@ -0,0 +1,13 @@
+#' Title
+#'
+#' @param df data that you are wanting to output a nice table for
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' #output_datatable(df)
+output_datatable<-function(df){
+
+  DT::datatable(df,extensions='Buttons',rownames=FALSE,filter=c("top"),options=list(dom='Bfrtip',buttons=c('csv')))
+}
