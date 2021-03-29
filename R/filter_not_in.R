eeda39232ba0c38e44599920a4fa5726d1db27c7
Patrick Keller <patrick@kelleranalytics.com>
2021-03-29 14:20:02 -0400

    fixed capitalization mistake

--- R/filter_not_in.R
+++ R/filter_not_in.R
@@ -39,5 +39,5 @@ filter_not_in<-function(.data,OtherDf,columnInData,columnInOtherDf){
   columnInOtherDf<-rlang::ensym(columnInOtherDf)
 
   .data%>%
-    filter({stopifnot(exists(as.character(columnInOtherDf), where = otherdf)); !(!!columnInData %in% otherdf[,names(otherdf) %in% c(columnInOtherDf)])})
+    filter({stopifnot(exists(as.character(columnInOtherDf), where = OtherDf)); !(!!columnInData %in% OtherDf[,names(OtherDf) %in% c(columnInOtherDf)])})
 }
